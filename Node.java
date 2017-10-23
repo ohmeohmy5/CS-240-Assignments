2ffd9dcd2782ec21579aef48a37f0790d6e3b9b9
Cesar <Cesar@10.110.49.228>
2017-10-23 12:34:13 -0700

    Node 1st attempt

--- Node.java
+++ Node.java
@@ -1,4 +1,15 @@
 
-public class Node {
-
+public class Node<T> {
+	private T data;
+	private Node next;
+	
+	public Node(T anObject, Node nextNode){
+		data = anObject;
+		next = nextNode;
+	}
+	
+	public T getData(){
+		return data;
+	}
+	
 }
