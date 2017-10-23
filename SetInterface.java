4a019aa6425d998d71700587ce655a5ee3895a06
Cesar <Cesar@QuadeComputer.home>
2017-10-23 00:12:53 -0700

    First attempt

--- SetInterface.java
+++ SetInterface.java
@@ -1,4 +1,11 @@
 
-public class SetInterface {
-
+public interface SetInterface<T> {
+	public boolean contains(T anObject);
+	public boolean remove(T anObject);
+	public boolean remove();
+	public boolean add(T anObject);
+	public int length();
+	public String toString();
+	public boolean isEmpty();
+	public void clear();
 }
